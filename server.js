c13c6109c89abf01cfa9c318562d5775505e4f83
raphaeljaoui <r.jaoui201@gmail.com>
2020-03-16 15:06:58 +0100

    hello

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 app.get('/', function(req,res){
     res.send('Hello world!')
 })
+app.get('/hello', function(req,res){
+    res.send('bonjour')
+})
+
 
 app.listen(3000, function(){
     console.log('port 3000')
