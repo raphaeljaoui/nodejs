a4ac5ae73d5262df7cb2803316c7d1d0a39d08b2
raphaeljaoui <r.jaoui201@gmail.com>
2020-03-16 16:27:33 +0100

    GET post

--- server.js
+++ server.js
@@ -16,40 +16,8 @@ app.get('/chat', function (req, res) {
     res.send('Je ne connais pas demain…');
   });
 app.post('/chat', function (req, res) {
-    if(req.body.msg != undefined){
-        var msg = req.body.msg
-        var index = msg.indexOf('=')
-        console.log(msg);
-        if(msg.indexOf('=') == -1){
-            fs.readFile("./reponses.json", (err, data) => {
-                if(data != undefined){
-                    if(data.length == 0){
-                        res.send("Je ne connais pas " + msg + ".");
-                    } else {
-                        if (err) throw err;
-                            let demain = JSON.parse(data);
-                            if(demain.demain !== undefined){
-                                res.send("demain: " + demain.demain)
-                            }
-                    }
-                } else {
-                    res.send("Je ne connais pas " + msg + ".");
-                }
-            });
-        } else {
-            let key = msg.substring(0, index)
-            let value = msg.substring(index+1, msg.length)
-            let content = {
-                demain: value
-            }
-            fs.writeFile("./reponses.json",JSON.stringify(content), error => {
-                if(error){
-                    console.error(error);
-                }
-                res.send("Merci pour cette information.")
-            })
-        }
-    }  });
+    res.send(req.body.msg);
+  });
 
 app.listen(3000, function(){
     console.log('port 3000')
