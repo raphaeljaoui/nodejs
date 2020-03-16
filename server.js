be194467e62e4dfc0ffe59e7ce2c981689bed9ec
raphaeljaoui <r.jaoui201@gmail.com>
2020-03-16 16:43:58 +0100

    GET post

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require('express')
-const app = express()
+const myApp = express()
 
-app.get('/', function(req,res){
+myApp.get('/', function(req,res){
     res.send('Hello world!')
 })
+myApp.get('/hello', function(req,res){
+    if(req.query.country == '' || req.query.country == undefined){
+           res.send('Which country?')
+       }
+       else
+       res.send('Bonjour, '+ req.query.country+'!')
+   })
 
-app.get("/hello", function(req,res){
-    if(req.query.nom == null){
-        res.send('Quel est votre nom ?')
-    }
-    res.send('Bonjour, '+ req.query.nom+' !')
-})
-
-app.get('/chat', function (req, res) {
-    res.send('Je ne connais pas demain…');
-  });
-app.post('/chat', function (req, res) {
-    res.send(req.body.msg);
-  });
-
-app.listen(3000, function(){
+myApp.listen(3000, function(){
     console.log('port 3000')
 })
