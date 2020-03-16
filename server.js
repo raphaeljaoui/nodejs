77184243a970237a37a5262295592182387b98a8
raphaeljaoui <r.jaoui201@gmail.com>
2020-03-16 15:20:48 +0100

    hello

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express()
 app.get('/', function(req,res){
     res.send('Hello world!')
 })
-app.get('/hello?nom=:id', function(req,res){
-    res.send('bonjour'+ req.params.id)
+app.get("/hello?nom=:id", function(req,res){
+    res.send('bonjour '+ req.params.id)
 })
 
 
