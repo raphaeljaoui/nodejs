a22246f0dbd2c43520766eea15554bc170daf856
raphaeljaoui <r.jaoui201@gmail.com>
2020-03-16 15:33:09 +0100

    hello

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express()
 app.get('/', function(req,res){
     res.send('Hello world!')
 })
-app.get("/hello", function(req,res){
-    res.send('Quel est votre nom ?')
-})
+// app.get("/hello", function(req,res){
+//     res.send('Quel est votre nom ?')
+// })
 
 app.get("/hello", function(req,res){
     res.send('bonjour, '+ req.query)
