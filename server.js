c64bae8f895fff8c55cd97e8f464c6d038d7c215
raphaeljaoui <r.jaoui201@gmail.com>
2020-03-16 12:05:43 +0100

    new push

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+
+app.get('/', function(req,res){
+    res.send('Hello world!')
+})
+
+app.listen(3000, function(){
+    console.log('port 3000')
+})
